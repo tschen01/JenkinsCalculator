def0ff960829cfa020850d108000398d363e5886
Scott Cheng <tschen015@gmail.com>
2021-02-18 15:16:26 -0700

    finish methods

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,4 @@
-
+import java.util.UUID;
 class Calculator {
 
     Calculator(){
@@ -39,7 +39,9 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        return 0;
+        if (n <= 1)
+            return n;
+        return fibonacciNumberFinder(n-1) + fibonacciNumberFinder(n-2);
     }
 
 
@@ -51,7 +53,7 @@ class Calculator {
     if int a = 16 then this method returns: 10000
      */
     String intToBinaryNumber(int n){
-        return null;
+        return Integer.toBinaryString(n);
     }
 
     /*
@@ -63,8 +65,8 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
-
-        return null;
+        String uniqueID = UUID.randomUUID().toString();
+        return n+uniqueID;
     }
 
 
